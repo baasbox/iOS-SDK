faef53d1a3336fb4e3a20bd018dcafd50f08b145
Cesare <cesarerocchi@gmail.com>
2014-03-18 13:59:33 +0100

    Testing imports

--- BaasBox-iOS-SDK/BAAClient.h
+++ BaasBox-iOS-SDK/BAAClient.h
@@ -24,114 +24,114 @@
 
 @interface BAAClient : NSObject
 
-@property (nonatomic, strong) BAAUser *currentUser;
-@property (nonatomic, strong, readonly) NSURL *baseURL;
-
-+ (instancetype)sharedClient;
-
-// Authentication
-- (void)authenticateUser:(NSString *)username
-                password:(NSString *)password
-              completion:(BAABooleanResultBlock)completionBlock;
-
-- (void)createUserWithUsername:(NSString *)username
-                      password:(NSString *)password
-                    completion:(BAABooleanResultBlock)completionBlock;
-
-- (BOOL) isAuthenticated;
-
-- (void) logoutWithCompletion:(BAABooleanResultBlock)completionBlock;
-
-// Loading
-- (void) loadObject:(BAAObject *)object completion:(BAAObjectResultBlock)completionBlock;
-- (void) loadCollection:(BAAObject *)object completion:(BAAArrayResultBlock)completionBlock;
-- (void) loadCollection:(BAAObject *)object withParams:(NSDictionary *)parameters completion:(BAAArrayResultBlock)completionBlock;
-
-// saving
-- (void) createObject:(BAAObject *)object completion:(BAAObjectResultBlock)completionBlock;
-- (void) updateObject:(BAAObject *)object completion:(BAAObjectResultBlock)completionBlock;
-
-// Deleting
-- (void) deleteObject:(BAAObject *)object completion:(BAABooleanResultBlock)completionBlock;
-
-// Push notifications
-- (void) askToEnablePushNotifications;
-- (void) enablePushNotifications:(NSData *)token completion:(BAABooleanResultBlock)completionBlock;
-- (void) disablePushNotificationsWithCompletion:(BAABooleanResultBlock)completionBlock;
-
-// Files
-- (void) loadFiles:(BAAFile *)file completion:(BAAArrayResultBlock)completionBlock;
-- (void) loadFiles:(BAAFile *)file withParams:(NSDictionary *) parameters completion:(BAAArrayResultBlock)completionBlock;
-- (NSURLSessionDataTask *) loadFileData:(BAAFile *)file completion:(void(^)(NSData *data, NSError *error))completionBlock;
-- (NSURLSessionDataTask *) loadFileData:(BAAFile *)file parameters:(NSDictionary *)parameters completion:(void(^)(NSData *data, NSError *error))completionBlock;
-- (void) uploadFile:(BAAFile *)file withPermissions:(NSDictionary *)permissions completion:(BAAObjectResultBlock)completionBlock;
-- (void) deleteFile:(BAAFile *)file completion:(BAABooleanResultBlock)completionBlock;
-- (void) loadFileDetails:(NSString *)fileID completion:(BAAObjectResultBlock)completionBlock;
-- (void) loadFilesAndDetailsWithCompletion:(BAAArrayResultBlock)completionBlock;
-
-// User
-- (void) loadCurrentUserWithCompletion:(BAAObjectResultBlock)completionBlock;
-- (void) updateUserWithCompletion:(BAAObjectResultBlock)completionBlock;
-- (void) loadUsersWithCompletion:(BAAArrayResultBlock)completionBlock;
-- (void) loadUsersWithParameters:(NSDictionary *)parameters completion:(BAAArrayResultBlock)completionBlock;
-- (void) loadUsersDetails:(NSString *) userId completion:(BAAObjectResultBlock)completionBlock;
-- (void) loadFollowingForUser:(BAAUser *)user completion:(BAAArrayResultBlock)completionBlock;
-- (void) loadFollowersOfUser:(BAAUser *)user completion:(BAAArrayResultBlock)completionBlock;
-- (void) followUser:(BAAUser *)user completion:(BAAObjectResultBlock)completionBlock;
-- (void) unfollowUser:(BAAUser *)user completion:(BAABooleanResultBlock)completionBlock;
-
-// Acl
-- (void) grantAccess:(BAAFile *)file toRole:(NSString *)roleName accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
-- (void) grantAccess:(BAAFile *)file toUser:(NSString *)username accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
-- (void) revokeAccess:(BAAFile *)file toRole:(NSString *)roleName accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
-- (void) revokeAccess:(BAAFile *)file toUser:(NSString *)username accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
-
-// Password
-- (void) changeOldPassword:(NSString *)oldPassword toNewPassword:(NSString *)newPassword completion:(BAABooleanResultBlock)completionBlock;
-- (void) resetPasswordForUser:(BAAUser *)user withCompletion:(BAABooleanResultBlock)completionBlock;
-
-// Admin
-- (void) createCollection:(NSString *)collectionName completion:(BAAObjectResultBlock)completionBlock;
-- (void) loadSettingsWithCompletion:(BAAObjectResultBlock)completionBlock;
-- (void) loadSettingsSection:(NSString *)sectionName completion:(BAAObjectResultBlock)completionBlock;
-- (void) setValue:(NSString *)value forKey:(NSString *)key inSection:(NSString *)sectionName completion:(BAAObjectResultBlock)completionBlock;
-
-// Core methods
-- (void)getPath:(NSString *)path
-     parameters:(NSDictionary *)parameters
-        success:(void (^)(id responseObject))success
-        failure:(void (^)(NSError *error))failure;
-
-- (void)postPath:(NSString *)path
-      parameters:(NSDictionary *)parameters
-         success:(void (^)(id responseObject))success
-         failure:(void (^)(NSError *error))failure;
-
-- (void)putPath:(NSString *)path
-     parameters:(NSDictionary *)parameters
-        success:(void (^)(id responseObject))success
-        failure:(void (^)(NSError *error))failure;
-    
-- (void)deletePath:(NSString *)path
-        parameters:(NSDictionary *)parameters
-           success:(void (^)(id responseObject))success
-           failure:(void (^)(NSError *error))failure;
-
-
-// Pagination constants
-extern NSString * const kPageNumberKey;
-extern NSString * const kPageSizeKey;
-extern NSInteger const kPageLength;
-
-// Role constants
-extern NSString * const kAclAnonymousRole;
-extern NSString * const kAclRegisteredRole;
-extern NSString * const kAclAdministratorRole;
-
-// ACL constants
-extern NSString * const kAclReadPermission;
-extern NSString * const kAclDeletePermission;
-extern NSString * const kAclUpdatePermission;
+// @property (nonatomic, strong) BAAUser *currentUser;
+// @property (nonatomic, strong, readonly) NSURL *baseURL;
+// 
+// + (instancetype)sharedClient;
+// 
+// // Authentication
+// - (void)authenticateUser:(NSString *)username
+//                 password:(NSString *)password
+//               completion:(BAABooleanResultBlock)completionBlock;
+// 
+// - (void)createUserWithUsername:(NSString *)username
+//                       password:(NSString *)password
+//                     completion:(BAABooleanResultBlock)completionBlock;
+// 
+// - (BOOL) isAuthenticated;
+// 
+// - (void) logoutWithCompletion:(BAABooleanResultBlock)completionBlock;
+// 
+// // Loading
+// - (void) loadObject:(BAAObject *)object completion:(BAAObjectResultBlock)completionBlock;
+// - (void) loadCollection:(BAAObject *)object completion:(BAAArrayResultBlock)completionBlock;
+// - (void) loadCollection:(BAAObject *)object withParams:(NSDictionary *)parameters completion:(BAAArrayResultBlock)completionBlock;
+// 
+// // saving
+// - (void) createObject:(BAAObject *)object completion:(BAAObjectResultBlock)completionBlock;
+// - (void) updateObject:(BAAObject *)object completion:(BAAObjectResultBlock)completionBlock;
+// 
+// // Deleting
+// - (void) deleteObject:(BAAObject *)object completion:(BAABooleanResultBlock)completionBlock;
+// 
+// // Push notifications
+// // - (void) askToEnablePushNotifications;
+// // - (void) enablePushNotifications:(NSData *)token completion:(BAABooleanResultBlock)completionBlock;
+// // - (void) disablePushNotificationsWithCompletion:(BAABooleanResultBlock)completionBlock;
+// 
+// // Files
+// - (void) loadFiles:(BAAFile *)file completion:(BAAArrayResultBlock)completionBlock;
+// - (void) loadFiles:(BAAFile *)file withParams:(NSDictionary *) parameters completion:(BAAArrayResultBlock)completionBlock;
+// - (NSURLSessionDataTask *) loadFileData:(BAAFile *)file completion:(void(^)(NSData *data, NSError *error))completionBlock;
+// - (NSURLSessionDataTask *) loadFileData:(BAAFile *)file parameters:(NSDictionary *)parameters completion:(void(^)(NSData *data, NSError *error))completionBlock;
+// - (void) uploadFile:(BAAFile *)file withPermissions:(NSDictionary *)permissions completion:(BAAObjectResultBlock)completionBlock;
+// - (void) deleteFile:(BAAFile *)file completion:(BAABooleanResultBlock)completionBlock;
+// - (void) loadFileDetails:(NSString *)fileID completion:(BAAObjectResultBlock)completionBlock;
+// - (void) loadFilesAndDetailsWithCompletion:(BAAArrayResultBlock)completionBlock;
+// 
+// // User
+// - (void) loadCurrentUserWithCompletion:(BAAObjectResultBlock)completionBlock;
+// - (void) updateUserWithCompletion:(BAAObjectResultBlock)completionBlock;
+// - (void) loadUsersWithCompletion:(BAAArrayResultBlock)completionBlock;
+// - (void) loadUsersWithParameters:(NSDictionary *)parameters completion:(BAAArrayResultBlock)completionBlock;
+// - (void) loadUsersDetails:(NSString *) userId completion:(BAAObjectResultBlock)completionBlock;
+// - (void) loadFollowingForUser:(BAAUser *)user completion:(BAAArrayResultBlock)completionBlock;
+// - (void) loadFollowersOfUser:(BAAUser *)user completion:(BAAArrayResultBlock)completionBlock;
+// - (void) followUser:(BAAUser *)user completion:(BAAObjectResultBlock)completionBlock;
+// - (void) unfollowUser:(BAAUser *)user completion:(BAABooleanResultBlock)completionBlock;
+// 
+// // Acl
+// - (void) grantAccess:(BAAFile *)file toRole:(NSString *)roleName accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
+// - (void) grantAccess:(BAAFile *)file toUser:(NSString *)username accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
+// - (void) revokeAccess:(BAAFile *)file toRole:(NSString *)roleName accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
+// - (void) revokeAccess:(BAAFile *)file toUser:(NSString *)username accessType:(NSString *)access completion:(BAAObjectResultBlock)completionBlock;
+// 
+// // Password
+// - (void) changeOldPassword:(NSString *)oldPassword toNewPassword:(NSString *)newPassword completion:(BAABooleanResultBlock)completionBlock;
+// - (void) resetPasswordForUser:(BAAUser *)user withCompletion:(BAABooleanResultBlock)completionBlock;
+// 
+// // Admin
+// - (void) createCollection:(NSString *)collectionName completion:(BAAObjectResultBlock)completionBlock;
+// - (void) loadSettingsWithCompletion:(BAAObjectResultBlock)completionBlock;
+// - (void) loadSettingsSection:(NSString *)sectionName completion:(BAAObjectResultBlock)completionBlock;
+// - (void) setValue:(NSString *)value forKey:(NSString *)key inSection:(NSString *)sectionName completion:(BAAObjectResultBlock)completionBlock;
+// 
+// // Core methods
+// - (void)getPath:(NSString *)path
+//      parameters:(NSDictionary *)parameters
+//         success:(void (^)(id responseObject))success
+//         failure:(void (^)(NSError *error))failure;
+// 
+// - (void)postPath:(NSString *)path
+//       parameters:(NSDictionary *)parameters
+//          success:(void (^)(id responseObject))success
+//          failure:(void (^)(NSError *error))failure;
+// 
+// - (void)putPath:(NSString *)path
+//      parameters:(NSDictionary *)parameters
+//         success:(void (^)(id responseObject))success
+//         failure:(void (^)(NSError *error))failure;
+//     
+// - (void)deletePath:(NSString *)path
+//         parameters:(NSDictionary *)parameters
+//            success:(void (^)(id responseObject))success
+//            failure:(void (^)(NSError *error))failure;
+// 
+// 
+// // Pagination constants
+// extern NSString * const kPageNumberKey;
+// extern NSString * const kPageSizeKey;
+// extern NSInteger const kPageLength;
+// 
+// // Role constants
+// extern NSString * const kAclAnonymousRole;
+// extern NSString * const kAclRegisteredRole;
+// extern NSString * const kAclAdministratorRole;
+// 
+// // ACL constants
+// extern NSString * const kAclReadPermission;
+// extern NSString * const kAclDeletePermission;
+// extern NSString * const kAclUpdatePermission;
 
 
 
--- BaasBoxSDK.podspec
+++ BaasBoxSDK.podspec
@@ -5,8 +5,7 @@ Pod::Spec.new do |spec|
   spec.homepage     = 'https://github.com/baasbox/iOS-SDK'
   spec.author       = { 'Cesare Rocchi' => 'c.rocchi@baasbox.com' }
   spec.source       = { :git => 'https://github.com/baasbox/iOS-SDK.git', :tag => "v0.7.4" }
-  spec.platform = :ios
-  spec.ios.deployment_target = "7.0"
+  spec.platform     = :ios
   spec.description  = 'iOS SDK for the open source back end BaasBox. See http://www.baasbox.com'
   spec.source_files = 'BaasBox-iOS-SDK/*.{h,m}'
   spec.requires_arc = true
