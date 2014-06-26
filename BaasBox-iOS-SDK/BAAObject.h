3e07513867adf460d43a27b85016c532b1f271aa
Cesare <cesarerocchi@gmail.com>
2014-06-26 11:50:22 +0200

    Marked designated initializer to make Swift happy

--- BaasBox-iOS-SDK/BAAObject.h
+++ BaasBox-iOS-SDK/BAAObject.h
@@ -22,7 +22,7 @@
 @property (nonatomic, assign, readonly) NSInteger version;
 @property (nonatomic, strong, readonly) NSDate *creationDate;
 
-- (instancetype) initWithDictionary:(NSDictionary *)dictionary;
+- (instancetype) initWithDictionary:(NSDictionary *)dictionary NS_DESIGNATED_INITIALIZER;
 
 + (void) getObjectsWithCompletion:(BAAArrayResultBlock)completionBlock;
 + (void) getObjectsWithParams:(NSDictionary *)parameters completion:(BAAArrayResultBlock)completionBlock;
